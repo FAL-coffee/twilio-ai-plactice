3b4e2e55bc85a95df8faac941b47d217281553fb
FAL-coffee <fal.engineer.2001@gmail.com>
2023-04-01 22:39:20 +0900

    :zap: 着信に対し、任意の文字列を発生するところまで実装 -Hello, World!

--- src/application/controllers/CallController.ts
+++ src/application/controllers/CallController.ts
@@ -1,64 +1,32 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Req,
-  Res,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller, Post, Body, Res, UseInterceptors } from '@nestjs/common';
 import { LoggerService, Context } from '@domain/services/logger.service';
-import { twilioClient } from '@infrastructure/adapters/output-adapters/twilio/twilio.client';
-import { Twilio } from 'twilio';
-import { handleIncomingCall } from '@infrastructure/adapters/output-adapters/twilio';
-import { twiml } from 'twilio';
+import { IncomingCallRequest } from '@application/interfaces/incoming-call-request.interface';
+import { handleIncomingCall } from '@infrastructure/adapters/output-adapters/twilio/twilio.call.handler';
 import { LoggingInterceptor } from '@application/interceptors/logging.interceptor';
 
 @Controller('call')
 @UseInterceptors(LoggingInterceptor)
 export class CallController {
-  private readonly twilioClient: Twilio;
   private Log: LoggerService = new LoggerService('createOperation');
-
-  constructor() {
-    this.twilioClient = twilioClient;
-  }
-
-  // getを定義
-  @Get()
-  public test(@Res() res: any) {
-    console.log('Hello World!');
-    res.send('Hello World!');
-  }
+  constructor() {}
 
   @Post()
-  public async handleCall(@Req() req: any, @Res() res: any) {
+  public async handleCall(
+    @Body() incomingCallRequest: IncomingCallRequest,
+    @Res() res: any,
+  ) {
     try {
-      const callSid = req.body.CallSid;
-      const callStatus = req.body.CallStatus;
-      const from = req.body.From;
-      const to = req.body.To;
+      // const callSid = req.body.CallSid;
+      // const callStatus = req.body.CallStatus;
+      const from = incomingCallRequest.From;
+      const to = incomingCallRequest.To;
 
       const context: Context = { module: 'CallController', method: 'post' };
       console.log(`Incoming call from ${from} to ${to}`);
-      this.Log.logger('Hello World!', context);
+      this.Log.logger('Incoming', context);
 
-      // if (callStatus === 'ringing') {
-      // const twiml = await handleIncomingCall();
-      // this.twilioClient.voice
-      //   // await this.twilioClient.calls(callSid).update({ twiml: twiml });
-
-      //   const callResponse = await handleIncomingCall(
-      //     new twiml.VoiceResponse(),
-      //   );
-      //   await this.twilioClient.calls(callSid).update({ twiml: callResponse });
-      // }
-      const resp = new twiml.VoiceResponse();
-      resp.say(
-        'こんにちわ、今日の天気はとても気持ちのいい晴れです。そろそろお昼ですね。',
-      );
-      res.send(resp.toString());
-
-      // res.send();
+      const callResponse = await handleIncomingCall();
+      res.send(callResponse);
     } catch (error) {
       console.error(error);
       res.status(500).send();
