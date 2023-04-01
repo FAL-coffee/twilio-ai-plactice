66f04dc31772b377dc1658e2f842ac80c79d894e
FAL-coffee <fal.engineer.2001@gmail.com>
2023-04-01 22:37:58 +0900

    :fire: 不要なコメントアウトを削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { CallController } from './application/controllers/CallController';
 import { LoggerMiddleware } from './application/middlewere/logger.middlewere';
-// import { TerminusModule } from '@nestjs/terminus';
-// import { TerminusOptionsService } from './infrastructure/health/terminus-options.check';
-
-// const HealthModule = TerminusModule.forRootAsync({
-//   useClass: TerminusOptionsService,
-// });
 
 @Module({
   imports: [],
--- src/infrastructure/adapters/output-adapters/twilio/twilio.call.handler.ts
+++ src/infrastructure/adapters/output-adapters/twilio/twilio.call.handler.ts
@@ -1,34 +1,6 @@
-import { twilioClient } from '@infrastructure/adapters/output-adapters/twilio/twilio.client';
-import Twilio, { twiml } from 'twilio';
+import { twiml } from 'twilio';
 
-export const startIncomingCall = async (
-  from: string,
-  to: string,
-  url: string,
-): Promise<void> => {
-  try {
-    await twilioClient.calls.create({
-      from,
-      to,
-      url,
-    });
-  } catch (err) {
-    throw new Error(`Failed to start incoming call: ${err.message}`);
-  }
-};
-
-export const stopIncomingCall = async (callSid: string): Promise<void> => {
-  try {
-    const call = await twilioClient.calls(callSid).fetch();
-    await call.update({ status: 'completed' });
-  } catch (err) {
-    throw new Error(`Failed to stop incoming call: ${err.message}`);
-  }
-};
-
-export const handleIncomingCall = async (
-  request: twiml.VoiceResponse,
-): Promise<string> => {
+export const handleIncomingCall = async (): Promise<string> => {
   const response = new twiml.VoiceResponse();
   response.say('Hello, World!');
 
